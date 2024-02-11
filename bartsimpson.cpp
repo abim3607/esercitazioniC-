d259e417dc30a9c61d5b9b53f15e107d24790cda
abim3607 <87835413+abim3607@users.noreply.github.com>
2024-02-11 18:27:15 +0100

    Add files via upload

---
+++ bartsimpson.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int q;
+	cout << "quante volte bart dovra scrivere la frase alla lavagna:" << endl;
+	cin >> q;
+	for (int i = q; i >= 0; i--){
+		//cout << i;
+		cout << i << ": bicchieri di birra"<< endl;
+		
+
+	}
+	//int i = 1;
+	//while (i <= q) {
+
+
+	//	cout << i;
+
+	//	cout << ": mi chiamo bartsimpson" << endl;
+	//	i++;
+	//	//i = i+1;
+	//}
+	/*int i = 1;
+	do {
+		cout << i;
+		cout << ": mi chiamo bartsimpson" << endl;
+
+		i = i+1;
+	} while (i <= q);*/
+	/*
+	1 mi chiamo bart simpsonm
+		1 la mia sorella e' lisasimpson
+		2 la mia sorella e' lisasimpson
+	2 mi chiamo bart simpson
+		1 la mia sorella e' lisasimpson
+		2  la mia sorella e' lisasimpson
+
+	*/
+	//int lisa;
+	//cout << "quante volte dovro dire lisa dopo bart detto: " << endl;
+	//cin >> lisa;
+	//for (int i = 1; i <= q; i++){
+	//	cout << "mi chiamo bart simpson"<< endl;
+	//	for (int w = 1; w <= lisa; w++)
+	//	{
+	//		cout << "la mia sorella e' lisasimpson" << endl;
+	//	}
+	//
+
+
+	//}
+	//int i = 1;
+	//int lisa;
+	//cout << "quante volte dovro dire lisa dopo bart detto: " << endl;
+	//cin >> lisa;
+	//while (i <=q){
+	//	
+	//	cout << i;
+	//	cout << "mi chiamo bart simpson" << endl;
+	//	int w = 1;
+	//	while (w <= lisa){
+	//		cout << w;
+	//		cout << "la mia sorella e' lisasimpson" << endl;
+	//		w++;
+	//	}
+	//	i++;
+	//}
+
+	system("pause");
+	return 0;
+}
